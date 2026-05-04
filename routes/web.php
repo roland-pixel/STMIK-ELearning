06f3829330353a36b53d5aafa2dde2102333d617
roland-pixel <kharis7181@gmail.com>
2026-05-05 00:22:35 +0700

    update lagi

--- routes/web.php
+++ routes/web.php
@@ -39,8 +39,12 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
-Route::get('/',  [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('/', [LoginController::class, 'login'])->name('login.post');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/login',  [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login'])->name('login.post');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 /*
